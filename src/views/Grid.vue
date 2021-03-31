faaea79cb907ebe4171858e62a40785c90202217
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-03-31 22:14:25 +0200

    Primer Commit

---
+++ src/views/Grid.vue
@@ -0,0 +1,24 @@
+<template>
+	<v-container>
+		<v-row>
+			<v-col cols="12" sm="6">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi quaerat ipsum velit
+					adipisci consequuntur voluptas, suscipit illo non iusto ratione molestias assumenda qui
+					molestiae magnam culpa illum beatae! Nam, incidunt.
+				</p>
+			</v-col>
+			<v-col cols="12" sm="6">
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi quaerat ipsum velit
+					adipisci consequuntur voluptas, suscipit illo non iusto ratione molestias assumenda qui
+					molestiae magnam culpa illum beatae! Nam, incidunt.
+				</p>
+			</v-col>
+		</v-row>
+	</v-container>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
