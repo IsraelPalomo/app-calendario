5d2b978cf485c81c0d51b67c129f7ee0c1a32e00
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-04-01 11:39:52 +0200

    Proyecto Terminado

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,26 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+	state: {
+		loading: {
+			titulo: "",
+			estado: false,
+			color: "primary",
+		},
+	},
+	mutations: {
+		mostrarLoading(state, payload) {
+			state.loading.titulo = payload.titulo;
+			state.loading.color = payload.color;
+			state.loading.estado = true;
+		},
+		ocultarLoading(state) {
+			state.loading.estado = false;
+		},
+	},
+	actions: {},
+	modules: {},
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,64 @@
 <template>
 	<div class="home">
-		<HelloWorld msg="Welcome to Your Vue.js App" />
+		<v-layout :wrap="true">
+			<v-flex xs12>
+				<v-card>
+					<v-date-picker
+						full-width
+						v-model="fecha"
+						locale="es"
+						:min="minimo"
+						:max="maximo"
+						@change="getDolar(fecha)"
+					></v-date-picker>
+				</v-card>
+				<v-card color="error" dark>
+					<v-card-text class="display-1 text-center">{{ valor }} </v-card-text>
+				</v-card>
+			</v-flex>
+		</v-layout>
 	</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
+import { mapMutations } from "vuex";
 export default {
-	name: "Home",
-	components: {
-		HelloWorld,
+	data() {
+		return {
+			fecha: new Date().toISOString().substr(0, 10),
+			minimo: "1999",
+			maximo: new Date().toISOString().substr(0, 10),
+			valor: null,
+		};
+	},
+	methods: {
+		...mapMutations(["mostrarLoading", "ocultarLoading"]),
+
+		async getDolar(dia) {
+			let arrayFecha = dia.split(["-"]);
+			let ddmmyy = arrayFecha[2] + "-" + arrayFecha[1] + "-" + arrayFecha[0];
+
+			try {
+				this.mostrarLoading({ titulo: "Accediendo a Informacion", color: "secondary" });
+				const datos = await axios.get(`https://mindicador.cl/api/euro/${ddmmyy}`);
+				if (datos.data.serie.length > 0) {
+					this.valor = await datos.data.serie[0].valor;
+				} else {
+					this.valor = "Sin Resultados";
+				}
+
+				console.log(datos.data.serie[0].valor);
+			} catch (error) {
+				console.log(error);
+			} finally {
+				this.ocultarLoading();
+			}
+		},
+	},
+	created() {
+		this.getDolar(this.fecha);
 	},
 };
 </script>
